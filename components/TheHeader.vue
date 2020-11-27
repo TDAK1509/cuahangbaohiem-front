2feff8ddf2944cc58b46d85330ca536de32de06c
Khuong Tran <dbcgdhh@gmail.com>
2020-11-27 18:19:38 +0700

    Init bulma header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,26 @@
 <template>
-  <header>Header</header>
+  <header class="px-6">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link class="navbar-item" to="/">
+          <img src="@/assets/images/logo.jpg" class="header__logo" />
+        </nuxt-link>
+
+        <a class="navbar-burger burger" data-target="navbarMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <nuxt-link class="navbar-item" to="/about">Về chúng tôi</nuxt-link>
+          <a class="navbar-item">Xe</a>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -7,3 +28,10 @@ export default {
   name: "Header"
 };
 </script>
+
+<style lang="scss" scoped>
+.header__logo {
+  height: 100px;
+  width: auto;
+}
+</style>
