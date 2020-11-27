749b1ba7ffd879b078645ed3636f9f451e02de9d
Khuong Tran <dbcgdhh@gmail.com>
2020-11-27 18:23:05 +0700

    Init bulma customize variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "cuahangbaohiem-front",
     meta: [
@@ -21,28 +19,16 @@ export default {
     ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ["@/assets/scss/main.scss"],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build"
-  ],
+  buildModules: ["@nuxt/typescript-build"],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    "@nuxtjs/bulma"
-  ],
+  modules: [],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     postcss: {
       preset: {
