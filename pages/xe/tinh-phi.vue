f36727658402ed95b86bc0494df3bb1332109de9
Khuong Tran <dbcgdhh@gmail.com>
2020-11-28 12:22:21 +0700

    Add test result not show if empty field

--- pages/xe/tinh-phi.vue
+++ pages/xe/tinh-phi.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <input data-cy="car-value" />
-    <input data-cy="car-year" />
-    <button data-cy="calculate-button">TÍNH PHÍ</button>
+    <input data-cy="car-value" v-model="carValue" />
+    <input data-cy="car-year" v-model="carYear" />
+    <button data-cy="calculate-button" @click="calculate">TÍNH PHÍ</button>
+
+    <div data-cy="result" v-if="hasResult">Result</div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "TinhPhiXe"
-};
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TinhPhiXe",
+
+  data() {
+    return {
+      hasResult: false,
+      carValue: null,
+      carYear: null
+    };
+  },
+
+  methods: {
+    calculate() {
+      if (!this.carValue || !this.carYear) {
+        return;
+      }
+
+      this.hasResult = true;
+    }
+  }
+});
 </script>
 
 <style>
--- test/e2e/integration/xe-tinh-phi.spec.js
+++ test/e2e/integration/xe-tinh-phi.spec.js
@@ -10,7 +10,41 @@ describe("Page /xe/tinh-phi", () => {
     });
 
     it("should render Calculate button", () => {
-      cy.get("[data-cy=calculate-button]").should("be.visible");
+      getCalculateButton().should("be.visible");
+    });
+  });
+
+  describe("on click calculate button", () => {
+    describe("error handling", () => {
+      it("if car value is empty, does not show result", () => {
+        getCarYearField().type("2015");
+        getCalculateButton().click();
+        cy.wait(1000);
+        assertResultBlockIsNotRendered();
+      });
+
+      it("if car year is empty, does not show result", () => {
+        getCarValueField().type("800");
+        getCalculateButton().click();
+        cy.wait(1000);
+        assertResultBlockIsNotRendered();
+      });
     });
   });
 });
+
+function getCarValueField() {
+  return cy.get("[data-cy=car-value]");
+}
+
+function getCarYearField() {
+  return cy.get("[data-cy=car-year]");
+}
+
+function getCalculateButton() {
+  return cy.get("[data-cy=calculate-button]");
+}
+
+function assertResultBlockIsNotRendered() {
+  cy.get("[data-cy=result]").should("not.empty");
+}
