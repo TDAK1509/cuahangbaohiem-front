8df860b0982df3fd96ab2d2f9362e272d434b778
Khuong Tran <dbcgdhh@gmail.com>
2020-11-30 21:36:47 +0700

    Add dummy content to index and about

--- pages/gioi-thieu.vue
+++ pages/gioi-thieu.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/">To home page</nuxt-link>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+        <h3>ABOUT US</h3>
+        <p v-for="i in 4" :key="i">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          temporibus suscipit corporis repudiandae molestiae consectetur
+          molestias eius, facilis minus perspiciatis accusantium blanditiis vel
+          culpa provident, enim recusandae nemo nostrum autem.
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
