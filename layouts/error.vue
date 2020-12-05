6af2ede4696e297ffe6ab87336c40bc7fbd2407d
Khuong Tran <dbcgdhh@gmail.com>
2020-12-05 20:35:01 +0700

    404 page the Nuxt way

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,11 @@
-
 <template>
   <section class="section">
     <div class="container">
-      <h5 class="title is-size-4">
+      <h5 v-if="error.statusCode === 404" class="title is-size-4">
         Rất tiếc, chúng tôi không tìm thấy nội dung bạn yêu cầu.
+      </h5>
+      <h5 v-else class="title is-size-4">
+        Đã có lỗi xảy ra (Mã {{ error.statusCode }})
       </h5>
       <nuxt-link to="/" class="button is-success">Trở về trang chủ</nuxt-link>
     </div>
@@ -12,6 +14,7 @@
 
 <script>
 export default {
-  name: "404"
+  props: ["error"],
+  layout: "error"
 };
 </script>
