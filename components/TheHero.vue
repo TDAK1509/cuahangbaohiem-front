8a4163674f48f9819c60cd550b213f077087e091
Tran Duy An Khuong <dbcgdhh@gmail.com>
2020-12-05 22:09:22 +0700

    Some minor fixes
    * Remove firebase analytics
    
    * Update hero temporarily use awake
    
    * Remove credit in footer
    
    * Add e2e:onetime
    
    * Update README

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="hero is-medium">
-    <img class="hero__img" src="~assets/images/hero.jpg" />
+    <img
+      class="hero__img"
+      src="https://awake-template.netlify.app/_nuxt/uploads/c55865a-640.jpg"
+      srcset="
+        https://awake-template.netlify.app/_nuxt/uploads/c55865a-640.jpg   640w,
+        https://awake-template.netlify.app/_nuxt/uploads/0761163-1027.jpg 1027w,
+        https://awake-template.netlify.app/_nuxt/uploads/c9769a1-1414.jpg 1414w,
+        https://awake-template.netlify.app/_nuxt/uploads/d4269c0-1800.jpg 1800w
+      "
+    />
 
     <div class="hero-body">
-      <div class="container">
-        <h1 class="title">Fullheight title</h1>
-        <h2 class="subtitle">Fullheight subtitle</h2>
-      </div>
+      <div class="container"></div>
     </div>
   </section>
 </template>
