0f2dd4a694ccb642f4b9ee96a0f64d5bdbdbe603
Khuong Tran <dbcgdhh@gmail.com>
2020-12-05 22:01:06 +0700

    Remove credit in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,11 +2,6 @@
   <footer class="footer has-background-dark has-text-white">
     <div class="is-flex">
       <p>v1.0</p>
-      <a
-        class="ml-4 footer__link"
-        href="https://www.freepik.com/photos/background"
-        >Background photo created by snowing - www.freepik.com</a
-      >
     </div>
   </footer>
 </template>
