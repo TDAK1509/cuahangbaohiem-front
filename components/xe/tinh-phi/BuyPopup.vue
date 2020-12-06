d2efd24175b9d0ff6f0e32f3fd5df650850fb755
Khuong Tran <dbcgdhh@gmail.com>
2020-12-06 07:32:29 +0700

    Update BuyPopup success view

--- components/xe/tinh-phi/BuyPopup.vue
+++ components/xe/tinh-phi/BuyPopup.vue
@@ -10,75 +10,76 @@
       </header>
 
       <section class="modal-card-body">
-        <p>
-          Vui lòng điền thông tin liên lạc của bạn, chúng tôi sẽ liên lạc với
-          bạn để tư vấn mua gói bảo hiểm bạn đã chọn.
+        <div v-if="!isSuccess">
+          <p>
+            Vui lòng điền thông tin liên lạc của bạn, chúng tôi sẽ liên lạc với
+            bạn để tư vấn mua gói bảo hiểm bạn đã chọn.
+          </p>
+
+          <form
+            class="mt-4"
+            name="car_insurance"
+            @submit.prevent="submit"
+            @keydown.esc="close"
+          >
+            <TextField
+              ref="name"
+              v-model="name"
+              data-cy="name"
+              label="Tên của bạn"
+              pattern="^[a-zA-Z\s]*$"
+              required
+            />
+
+            <TextField
+              v-model="email"
+              data-cy="email"
+              label="Email của bạn"
+              type="email"
+              name="email"
+              required
+            />
+
+            <TextField
+              v-model="phone"
+              data-cy="phone"
+              label="Số điện thoại của bạn"
+              type="tel"
+              name="phone"
+              pattern="^[0-9]*$"
+              required
+            />
+
+            <div class="field">
+              <label class="label">Lời nhắn của bạn</label>
+              <div class="control">
+                <textarea
+                  v-model="note"
+                  data-cy="note"
+                  class="textarea"
+                  name="note"
+                ></textarea>
+              </div>
+            </div>
+
+            <div class="field mt-4">
+              <div class="control">
+                <button
+                  data-cy="popup-buy-button"
+                  class="button is-info"
+                  :class="{ 'is-loading': loading }"
+                >
+                  XÁC NHẬN
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <p v-else>
+          Cám ơn bạn đã lựa chọn dịch vụ của chúng tôi. Chúng tôi sẽ liên lạc
+          với bạn trong thời gian sớm nhất!
         </p>
-
-        <form
-          class="mt-4"
-          name="car_insurance"
-          @submit.prevent="submit"
-          @keydown.esc="close"
-        >
-          <TextField
-            ref="name"
-            v-model="name"
-            data-cy="name"
-            label="Tên của bạn"
-            pattern="^[a-zA-Z\s]*$"
-            required
-          />
-
-          <TextField
-            v-model="email"
-            data-cy="email"
-            label="Email của bạn"
-            type="email"
-            name="email"
-            required
-          />
-
-          <TextField
-            v-model="phone"
-            data-cy="phone"
-            label="Số điện thoại của bạn"
-            type="tel"
-            name="phone"
-            pattern="^[0-9]*$"
-            required
-          />
-
-          <div class="field">
-            <label class="label">Lời nhắn của bạn</label>
-            <div class="control">
-              <textarea
-                v-model="note"
-                data-cy="note"
-                class="textarea"
-                name="note"
-              ></textarea>
-            </div>
-          </div>
-
-          <div class="field mt-4">
-            <div class="control">
-              <button
-                v-if="!isSuccess"
-                data-cy="popup-buy-button"
-                class="button is-info"
-                :class="{ 'is-loading': loading }"
-              >
-                XÁC NHẬN
-              </button>
-
-              <p v-else class="has-text-success">
-                Cám ơn bạn đã lựa chọn dịch vụ của chúng tôi. Chúng tôi sẽ liên
-                lạc với bạn trong thời gian sớm nhất!
-              </p>
-            </div>
-          </div>
-        </form>
       </section>
     </div>
   </div>
