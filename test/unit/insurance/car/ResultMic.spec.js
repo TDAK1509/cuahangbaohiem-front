645d4a4fda7eda512a1a9a3fef31b50a0ca263e8
Khuong Tran <dbcgdhh@gmail.com>
2020-12-10 19:25:04 +0700

    Fix unit test

--- test/unit/insurance/car/ResultMic.spec.js
+++ test/unit/insurance/car/ResultMic.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from "@vue/test-utils";
-import ResultPvi from "@/components/xe/tinh-phi/ResultPvi.vue";
+import ResultMic from "@/components/insurance/car/ResultMic.vue";
 import MockDate from "mockdate";
 
-describe("ResultPvi.vue", () => {
+describe("ResultMic.vue", () => {
   describe("Insurance Value", () => {
     beforeAll(() => {
       MockDate.set(new Date(2020, 1, 1));
@@ -13,36 +13,36 @@ describe("ResultPvi.vue", () => {
     describe("carValue is < 500", () => {
       const carValue = 400;
 
-      it("given carYear's gap from today <= 3, returns 1.7% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today <= 3, returns 1.23% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 3
           }
         });
-        const expectInsuranceValue = (carValue * 1.7) / 100;
+        const expectInsuranceValue = (carValue * 1.23) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today > 3 & <=6, returns 2.05% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today > 3 & <=6, returns 1.3% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 4
           }
         });
-        const expectInsuranceValue = (carValue * 2.05) / 100;
+        const expectInsuranceValue = (carValue * 1.3) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today >= 6, returns 2.35% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today >= 6, returns 1.38% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 7
           }
         });
-        const expectInsuranceValue = (carValue * 2.35) / 100;
+        const expectInsuranceValue = (carValue * 1.38) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
     });
@@ -50,36 +50,36 @@ describe("ResultPvi.vue", () => {
     describe("500 <= carValue < 700", () => {
       const carValue = 600;
 
-      it("given carYear's gap from today <= 3, returns 1.45% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today <= 3, returns 1.23% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 3
           }
         });
-        const expectInsuranceValue = (carValue * 1.45) / 100;
+        const expectInsuranceValue = (carValue * 1.23) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today > 3 & <=6, returns 1.74% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today > 3 & <=6, returns 1.3% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 4
           }
         });
-        const expectInsuranceValue = (carValue * 1.74) / 100;
+        const expectInsuranceValue = (carValue * 1.3) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today >= 6, returns 2% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today >= 6, returns 1.38% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 7
           }
         });
-        const expectInsuranceValue = (carValue * 2) / 100;
+        const expectInsuranceValue = (carValue * 1.38) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
     });
@@ -87,36 +87,36 @@ describe("ResultPvi.vue", () => {
     describe("carValue >= 700", () => {
       const carValue = 800;
 
-      it("given carYear's gap from today <= 3, returns 1.28% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today <= 3, returns 1.23% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 3
           }
         });
-        const expectInsuranceValue = (carValue * 1.28) / 100;
+        const expectInsuranceValue = (carValue * 1.23) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today > 3 & <=6, returns 1.54% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today > 3 & <=6, returns 1.3% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 4
           }
         });
-        const expectInsuranceValue = (carValue * 1.54) / 100;
+        const expectInsuranceValue = (carValue * 1.3) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
 
-      it("given carYear's gap from today >= 6, returns 1.76% carValue", () => {
-        const wrapper = shallowMount(ResultPvi, {
+      it("given carYear's gap from today >= 6, returns 1.38% carValue", () => {
+        const wrapper = shallowMount(ResultMic, {
           propsData: {
             carValue,
             carYear: thisYear - 7
           }
         });
-        const expectInsuranceValue = (carValue * 1.76) / 100;
+        const expectInsuranceValue = (carValue * 1.38) / 100;
         expect(wrapper.vm.insuranceValue).toBe(expectInsuranceValue);
       });
     });
