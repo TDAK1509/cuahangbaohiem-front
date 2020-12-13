1c9b86ffdcf26bb51157afac333c8fc377137887
Khuong Tran <dbcgdhh@gmail.com>
2020-12-13 19:23:13 +0700

    Add tests and pages ung-thu

---
+++ pages/san-pham/ung-thu/khuyen-mai.vue
@@ -0,0 +1,17 @@
+<router>
+{
+  name: "khuyen-mai_ung-thu"
+}
+</router>
+
+<template>
+  <div>khuyeanb mai</div>
+</template>
+
+<script>
+export default {
+  name: "KhuyenMaiTaiNan",
+
+  layout: "calculator"
+};
+</script>
