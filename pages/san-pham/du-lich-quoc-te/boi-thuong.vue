0237b39f98a357601796c528bc1bfc07d290693c
Khuong Tran <dbcgdhh@gmail.com>
2020-12-13 19:46:23 +0700

    Add tests and pages du-lich-quoc-te

---
+++ pages/san-pham/du-lich-quoc-te/boi-thuong.vue
@@ -0,0 +1,17 @@
+<router>
+{
+  name: "boi-thuong_du-lich-quoc-te"
+}
+</router>
+
+<template>
+  <div>boi-thuong</div>
+</template>
+
+<script>
+export default {
+  name: "BoiThuongTaiNan",
+
+  layout: "calculator"
+};
+</script>
