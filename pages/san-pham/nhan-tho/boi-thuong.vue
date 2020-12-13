a523f152127883f39fa1351b8e51d4edafb29d26
Khuong Tran <dbcgdhh@gmail.com>
2020-12-13 19:52:00 +0700

    Add tests and pages nhan-tho

---
+++ pages/san-pham/nhan-tho/boi-thuong.vue
@@ -0,0 +1,17 @@
+<router>
+{
+  name: "boi-thuong_nhan-tho"
+}
+</router>
+
+<template>
+  <div>boi-thuong</div>
+</template>
+
+<script>
+export default {
+  name: "BoiThuongTaiNan",
+
+  layout: "calculator"
+};
+</script>
---
+++ pages/san-pham/nhan-tho/khuyen-mai.vue
@@ -0,0 +1,17 @@
+<router>
+{
+  name: "khuyen-mai_nhan-tho"
+}
+</router>
+
+<template>
+  <div>khuyeanb mai</div>
+</template>
+
+<script>
+export default {
+  name: "KhuyenMaiTaiNan",
+
+  layout: "calculator"
+};
+</script>
