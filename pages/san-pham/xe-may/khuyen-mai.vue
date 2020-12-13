472ef9d27d983ec6d6a20418ff60615e9e46683d
Khuong Tran <dbcgdhh@gmail.com>
2020-12-13 19:41:09 +0700

    Add tests and pages xe-may

---
+++ pages/san-pham/xe-may/khuyen-mai.vue
@@ -0,0 +1,17 @@
+<router>
+{
+  name: "khuyen-mai_xe-may"
+}
+</router>
+
+<template>
+  <div>khuyeanb mai</div>
+</template>
+
+<script>
+export default {
+  name: "KhuyenMaiTaiNan",
+
+  layout: "calculator"
+};
+</script>
