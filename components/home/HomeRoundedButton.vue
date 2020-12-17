9983761940f4e4daf4773bb26567c8ccaabccd3f
Khuong Tran <dbcgdhh@gmail.com>
2020-12-17 19:27:11 +0700

    Style button size and position

--- components/home/HomeRoundedButton.vue
+++ components/home/HomeRoundedButton.vue
@@ -18,13 +18,17 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/scss/_colors";
 
+$button-size: 160px;
+
 .home-rounded-button {
   border-radius: 50%;
   border: 2px solid $grey;
-  width: 100px;
-  height: 100px;
+  width: $button-size;
+  height: $button-size;
   display: flex;
   align-items: center;
   justify-content: center;
+  text-align: center;
+  padding: 0.7rem;
 }
 </style>
--- components/home/HomeRoundedButtonContainer.vue
+++ components/home/HomeRoundedButtonContainer.vue
@@ -11,8 +11,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~assets/scss/_breakpoints";
+
 .home-rounded-button-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-items: center;
+  row-gap: 3rem;
+  max-width: 900px;
+  margin: auto;
+}
+
+@media only screen and (max-width: $mobile) {
+  .home-rounded-button-container {
+    grid-template-columns: 1fr 1fr;
+    row-gap: 1.5rem;
+  }
 }
 </style>
