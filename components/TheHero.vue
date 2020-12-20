65b16ae3946fac2fd46e8505d157751234c71990
Khuong Tran <dbcgdhh@gmail.com>
2020-12-20 10:36:20 +0700

    Add image background to hero

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,9 +1,13 @@
 <template>
   <section class="hero is-medium is-dark">
     <div class="hero-body">
+      <img src="~assets/images/hero.jpg" class="hero__img" />
+
       <div class="container">
-        <h1 class="title hero__title has-text-white mb-2">HOTLINE:</h1>
-        <h1 class="title hero__title has-text-warning">0123456789</h1>
+        <div class="hero__hotline-container">
+          <h1 class="title hero__title has-text-black mb-1">Hotline:</h1>
+          <h1 class="title hero__title has-text-white">0123456789</h1>
+        </div>
       </div>
     </div>
   </section>
@@ -21,6 +25,23 @@ export default {
 
 .hero {
   z-index: $z-index-hero;
+}
+
+.hero-body {
+  position: relative;
+}
+
+.hero__img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.hero__hotline-container {
+  text-align: right;
 }
 
 @media only screen and (max-width: $mobile) {
