93e02b0ba01cbd43f9806f9a76057e980057b186
Khuong Tran <dbcgdhh@gmail.com>
2020-12-20 10:28:24 +0700

    Add hot line to hero

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -2,8 +2,8 @@
   <section class="hero is-medium is-dark">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">Primary bold title</h1>
-        <h2 class="subtitle">Primary bold subtitle</h2>
+        <h1 class="title hero__title has-text-white mb-2">HOTLINE:</h1>
+        <h1 class="title hero__title has-text-warning">0123456789</h1>
       </div>
     </div>
   </section>
@@ -17,8 +17,15 @@ export default {
 
 <style lang="scss" scoped>
 @import "~assets/scss/_z-index";
+@import "~assets/scss/_breakpoints";
 
 .hero {
   z-index: $z-index-hero;
+}
+
+@media only screen and (max-width: $mobile) {
+  .hero__title {
+    font-size: 1.5rem;
+  }
 }
 </style>
