66692637ef447d929ec574e06e1765cd0c78c377
Khuong Tran <dbcgdhh@gmail.com>
2021-01-27 19:31:07 +0700

    Edit header navs

--- components/TheHeaderNav.vue
+++ components/TheHeaderNav.vue
@@ -6,7 +6,7 @@
       </span>
     </div>
 
-    <ul v-show="showNav" class="header-nav__ul">
+    <ul class="header-nav__ul--desktop">
       <li class="header-nav__li">
         <nuxt-link class="header__single-link header__link" to="/gioi-thieu">
           Giới thiệu
@@ -14,35 +14,22 @@
       </li>
 
       <li class="header-nav__li">
-        <div class="header-nav__dropdown">
-          <div class="header-nav__dropdown-label">
-            Sản phẩm
+        <nuxt-link class="header__single-link header__link" to="/san-pham">
+          Sản phẩm
+        </nuxt-link>
+      </li>
+    </ul>
 
-            <span class="icon has-text-dark">
-              <i class="fas fa-angle-down"></i>
-            </span>
-          </div>
-
-          <ul class="header-nav__dropdown-ul">
-            <li v-for="(nav, index) in navs" :key="index">
-              <nuxt-link
-                :data-cy="nav.dataCy"
-                :to="nav.to"
-                class="header-nav__dropdown-link header__link"
-              >
-                <span class="icon is-medium">
-                  <i :class="nav.iconClass" />
-                </span>
-                <span class="is-size-6">{{ nav.text }}</span>
-              </nuxt-link>
-            </li>
-          </ul>
-        </div>
+    <ul v-show="showNav" class="header-nav__ul--mobile">
+      <li class="header-nav__li">
+        <nuxt-link class="header__single-link header__link" to="/gioi-thieu">
+          Giới thiệu
+        </nuxt-link>
       </li>
 
       <li class="header-nav__li">
-        <nuxt-link class="header__single-link header__link" to="/huong-dan">
-          Hướng dẫn
+        <nuxt-link class="header__single-link header__link" to="/san-pham">
+          Sản phẩm
         </nuxt-link>
       </li>
     </ul>
@@ -50,44 +37,17 @@
 </template>
 
 <script>
-import { MOBILE_BREAKPOINT } from "@/utils/breakpoint";
-import debounce from "lodash/debounce";
-import InsuranceNavs from "@/utils/insurance-navs";
-
-const insuranceNavs = new InsuranceNavs();
-
 export default {
   name: "HeaderNav",
 
   data() {
     return {
-      showNav: false,
-      navs: insuranceNavs.headerNavs,
-      onWindowResize: () => {}
+      showNav: false
     };
   },
 
   mounted() {
-    const vm = this;
-
-    function updateNavBehavior() {
-      const isMobile = window.innerWidth < MOBILE_BREAKPOINT;
-      if (isMobile) {
-        vm.closeNav();
-        vm.closeNavOnRouteChange();
-      } else {
-        vm.showNav = true;
-      }
-    }
-
-    updateNavBehavior();
-    this.onWindowResize = debounce(updateNavBehavior, 300);
-
-    window.addEventListener("resize", this.onWindowResize);
-  },
-
-  beforeDestroy() {
-    window.removeEventListener("resize", this.onWindowResize);
+    this.closeNavOnRouteChange();
   },
 
   methods: {
@@ -100,10 +60,10 @@ export default {
     },
 
     closeNavOnRouteChange() {
-      const dropdownLinks = this.$refs.nav.querySelectorAll(".header__link");
+      const routeLinkDoms = this.$refs.nav.querySelectorAll(".header__link");
 
-      dropdownLinks.forEach((dropdownLink) => {
-        dropdownLink.addEventListener("click", () => {
+      routeLinkDoms.forEach((routeLinkDom) => {
+        routeLinkDom.addEventListener("click", () => {
           this.closeNav();
         });
       });
@@ -125,10 +85,14 @@ export default {
   display: none;
 }
 
-.header-nav__ul {
+.header-nav__ul--desktop {
   display: flex;
   align-items: center;
   height: 100%;
+}
+
+.header-nav__ul--mobile {
+  display: none;
 }
 
 .header-nav__li {
@@ -143,57 +107,11 @@ export default {
 
   &.is-active {
     color: $link;
+    font-weight: 700;
   }
 
   &:hover {
     color: $link;
-  }
-}
-
-.header-nav__dropdown {
-  position: relative;
-  height: 100%;
-  display: flex;
-  align-items: center;
-}
-
-.header-nav__dropdown-label {
-  cursor: default;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.header-nav__dropdown-ul {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  visibility: hidden;
-}
-
-.header-nav__dropdown:hover .header-nav__dropdown-ul {
-  visibility: visible;
-}
-
-.header-nav__dropdown-link {
-  color: inherit;
-  padding: 8px 8px;
-  background: #fff;
-  width: 300px;
-  display: block;
-
-  &:hover {
-    background: $grey-lighter;
-    cursor: pointer;
-  }
-
-  &.is-active,
-  &.is-active:hover {
-    background: $primary;
-    color: #fff;
-    cursor: default;
   }
 }
 
@@ -214,7 +132,11 @@ export default {
     height: 100%;
   }
 
-  .header-nav__ul {
+  .header-nav__ul--desktop {
+    display: none;
+  }
+
+  .header-nav__ul--mobile {
     display: block;
     background: #fff;
     position: fixed;
@@ -229,34 +151,6 @@ export default {
     height: auto;
     margin-left: 0;
     border-bottom: 1px solid $border-color-mobile;
-  }
-
-  .header-nav__dropdown {
-    display: block;
-    text-align: left;
-    width: 100%;
-  }
-
-  .header-nav__dropdown-label {
-    justify-content: flex-start;
-    padding: 15px;
-
-    .icon {
-      display: none;
-    }
-  }
-
-  .header-nav__dropdown-ul {
-    display: block;
-    position: static;
-    top: 0;
-    left: 0;
-    visibility: visible;
-  }
-
-  .header-nav__dropdown-link {
-    padding: 15px;
-    width: 100%;
   }
 
   .header__single-link {
