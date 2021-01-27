1a67097744e7b6de337a765578c3d5dd6c6d6c11
Khuong Tran <dbcgdhh@gmail.com>
2021-01-27 19:31:00 +0700

    Init product page

---
+++ pages/san-pham/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <section class="section">
+      <div class="container">Product page</div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Product",
+
+  head() {
+    return {
+      title: "Sản phẩm | Cửa hàng bảo hiểm"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
