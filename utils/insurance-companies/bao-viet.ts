cf500d6088d9e9d87f72e2f507eb1679733568d7
Khuong Tran <dbcgdhh@gmail.com>
2021-02-11 10:49:19 +0700

    Clone class Pvi into BaoViet

---
+++ utils/insurance-companies/bao-viet.ts
@@ -0,0 +1,19 @@
+import { CarYearThreshold } from "@/utils/car";
+
+export default class BaoViet {
+  public getCarInsuranceValue(
+    carValue: number,
+    carYearThreshold: CarYearThreshold
+  ): number {
+    switch (carYearThreshold) {
+      case CarYearThreshold.LESS_THAN_OR_EQUAL_3_YEARS:
+        return carValue * 1.2;
+      case CarYearThreshold.FROM_3_TO_6_YEARS:
+        return carValue * 1.3;
+      case CarYearThreshold.OVER_6_YEARS:
+        return carValue * 1.4;
+      default:
+        return 0;
+    }
+  }
+}
