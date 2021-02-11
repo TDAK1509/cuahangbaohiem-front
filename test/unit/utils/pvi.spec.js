a93044c5ee658dfecb00b9ca9c16fb64beac26b7
Khuong Tran <dbcgdhh@gmail.com>
2021-02-11 14:16:07 +0700

    Restructure test pvi

--- test/unit/utils/pvi.spec.js
+++ test/unit/utils/pvi.spec.js
@@ -3,37 +3,43 @@ import { CarYearThreshold } from "@/utils/car";
 
 describe("class Pvi", () => {
   describe("getCarInsuranceValue()", () => {
-    it("if car year threshold is <= 3 years, returns car value * 1.2", () => {
-      const carValue = 100;
-      const pvi = new Pvi();
-      const carInsuranceValue = pvi.getCarInsuranceValue(
-        carValue,
-        CarYearThreshold.LESS_THAN_OR_EQUAL_3_YEARS
-      );
-      const expectedCarInsuranceValue = carValue * 1.2;
-      expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+    describe("if car year threshold is <= 3 years", () => {
+      it("if no addons, returns car value * 1.2", () => {
+        const carValue = 100;
+        const pvi = new Pvi();
+        const carInsuranceValue = pvi.getCarInsuranceValue(
+          carValue,
+          CarYearThreshold.LESS_THAN_OR_EQUAL_3_YEARS
+        );
+        const expectedCarInsuranceValue = carValue * 1.2;
+        expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+      });
     });
 
-    it("if 3 < car year threshold <= 6, returns car value * 1.3", () => {
-      const carValue = 100;
-      const pvi = new Pvi();
-      const carInsuranceValue = pvi.getCarInsuranceValue(
-        carValue,
-        CarYearThreshold.FROM_3_TO_6_YEARS
-      );
-      const expectedCarInsuranceValue = carValue * 1.3;
-      expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+    describe("if 3 < car year threshold <= 6", () => {
+      it("if no addons, returns car value * 1.3", () => {
+        const carValue = 100;
+        const pvi = new Pvi();
+        const carInsuranceValue = pvi.getCarInsuranceValue(
+          carValue,
+          CarYearThreshold.FROM_3_TO_6_YEARS
+        );
+        const expectedCarInsuranceValue = carValue * 1.3;
+        expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+      });
     });
 
-    it("if 6 < car year threshold <= 10, returns car value * 1.4", () => {
-      const carValue = 100;
-      const pvi = new Pvi();
-      const carInsuranceValue = pvi.getCarInsuranceValue(
-        carValue,
-        CarYearThreshold.OVER_6_YEARS
-      );
-      const expectedCarInsuranceValue = carValue * 1.4;
-      expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+    describe("if 6 < car year threshold <= 10", () => {
+      it("if no addons, returns car value * 1.4", () => {
+        const carValue = 100;
+        const pvi = new Pvi();
+        const carInsuranceValue = pvi.getCarInsuranceValue(
+          carValue,
+          CarYearThreshold.OVER_6_YEARS
+        );
+        const expectedCarInsuranceValue = carValue * 1.4;
+        expect(carInsuranceValue).toBe(expectedCarInsuranceValue);
+      });
     });
 
     it("if car year threshold > 10, returns 0", () => {
