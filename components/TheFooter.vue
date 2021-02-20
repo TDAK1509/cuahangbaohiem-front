7b92998a838c38031ef8da74e8bf4d0a64360e01
Khuong Tran <dbcgdhh@gmail.com>
2021-02-20 21:19:17 +0700

    Move first column to new .vue file

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,28 +1,8 @@
 <template>
   <footer class="footer has-background-dark has-text-white">
     <div class="columns">
-      <div class="column">
-        <figure class="image">
-          <img class="image footer__image" src="~assets/images/logo.png" />
-        </figure>
+      <FooterFirstColumn />
 
-        <ul class="footer__list is-size-7 ml-4 mt-4">
-          <li class="mt-1">Công ty cổ phần Cửa Hàng Bảo Hiểm</li>
-          <li class="mt-1">
-            Số 69, đường Lê Lai, phường Lê Lợi,<br />Thành phố Hồ Chí Minh
-          </li>
-
-          <li class="mt-1">
-            Hotline: <a class="footer__link" href="tel:12345">12345</a>
-          </li>
-          <li class="mt-1">
-            Email:
-            <a class="footer__link" href="mailto:info@cuahangbaohiem.com"
-              >info@cuahangbaohiem.com</a
-            >
-          </li>
-        </ul>
-      </div>
       <div class="column">
         <h4 class="is-size-5">COLUMN 2</h4>
       </div>
@@ -43,18 +23,5 @@ export default {
 .footer__image {
   height: 40px;
   width: auto;
-}
-
-.footer__list {
-  list-style-type: disc;
-}
-
-.footer__link {
-  color: yellow;
-
-  &:hover {
-    color: #fff;
-    text-decoration: underline;
-  }
 }
 </style>
---
+++ components/footer/FooterFirstColumn.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="column">
+    <figure class="image">
+      <img
+        class="image footer-first-column__image"
+        src="~assets/images/logo.png"
+      />
+    </figure>
+
+    <ul class="footer-first-column__list is-size-7 ml-4 mt-4">
+      <li class="mt-1">Công ty cổ phần Cửa Hàng Bảo Hiểm</li>
+      <li class="mt-1">
+        Số 69, đường Lê Lai, phường Lê Lợi,<br />Thành phố Hồ Chí Minh
+      </li>
+
+      <li class="mt-1">
+        Hotline: <a class="footer-first-column__link" href="tel:12345">12345</a>
+      </li>
+      <li class="mt-1">
+        Email:
+        <a
+          class="footer-first-column__link"
+          href="mailto:info@cuahangbaohiem.com"
+          >info@cuahangbaohiem.com</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterFirstColumn"
+};
+</script>
+
+<style lang="scss" scoped>
+.footer-first-column__image {
+  height: 40px;
+  width: auto;
+}
+
+.footer-first-column__list {
+  list-style-type: disc;
+}
+
+.footer-first-column__link {
+  color: yellow;
+
+  &:hover {
+    color: #fff;
+    text-decoration: underline;
+  }
+}
+</style>
