97c9f06ac8a9eca270a4678928e1d3b645fe530d
Khuong Tran <dbcgdhh@gmail.com>
2021-03-03 21:29:02 +0700

    Add content to boi thuong page

--- pages/san-pham/o-to/boi-thuong.vue
+++ pages/san-pham/o-to/boi-thuong.vue
@@ -5,7 +5,143 @@
 </router>
 
 <template>
-  <div>boi-thuong</div>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+        <h3>BỒI THƯỜNG BẢO HIỂM VẬT CHẤT XE Ô TÔ</h3>
+
+        <p>
+          Khi không may xảy ra tổn thất đối với xe, quý khách thực hiện các bước
+          sau
+        </p>
+
+        <h5>Bước 1:</h5>
+
+        <p>
+          Thông báo ngay cho Bảo hiểm PVI qua đường dây nóng 1900545458 và thực
+          hiện theo các chỉ dẫn của Bảo hiểm PVI.
+        </p>
+
+        <p>
+          Đồng thời, quý khách thực hiện ngay việc làm cần thiết để ngăn ngừa
+          tổn thất xảy ra thêm như cứu chữa, hạn chế thiệt hại về người và tài
+          sản, bảo vệ hiện trường; đồng thời thông báo cho cơ quan Công an hoặc
+          chính quyền địa phương nơi gần nhất (trừ trường hợp bất khả kháng).
+        </p>
+
+        <h5>Bước 2:</h5>
+
+        <p>
+          Đưa xe vào garage sửa chữa theo chỉ định của Bảo hiểm PVI hoặc garage
+          quý khách yêu cầu trong trường hợp quý khách tham gia điều kiện bổ
+          sung 007/XCG-PVI: Lựa chọn cơ sở sửa chữa chính hãng.
+        </p>
+
+        <h5>Bước 3:</h5>
+
+        <p>
+          Điền vào thông báo tai nạn và Yêu cầu bồi thường theo mẫu của Bảo hiểm
+          PVI (<a href="/documents/pvi/boi-thuong-o-to.docx" download
+            >tải tại đây</a
+          >)
+        </p>
+
+        <p>
+          Đọc, thống nhất và ký biên bản giám định do cán bộ giám định của Bảo
+          hiểm lập.
+        </p>
+
+        <p>
+          Cung cấp các giấy tờ cần thiết theo yêu cầu của cán bộ giám định của
+          Bảo hiểm PVI gồm: Đăng ký xe, chứng nhận bảo hiểm, sổ đăng kiểm, giấy
+          phép lái xe của người điều khiển xe khi xe gặp tai nạn.
+        </p>
+
+        <p>
+          Phối hợp với Bảo hiểm PVI thu thập các tài liệu liên quan đến vụ tai
+          nạn của cơ quan công an.
+        </p>
+
+        <p>
+          Cung cấp các tài liệu cần thiết theo hướng dẫn của Bảo hiểm PVI để
+          chuyển quyền đòi bồi thường từ bên gây thiệt hại cho xe được bảo hiểm
+          trong trường hợp thiệt hại liên quan đến lỗi của bên thứ ba. Trường
+          hợp quý khách không thực hiện việc chuyển quyền đòi bồi thường hoặc tự
+          thỏa thuận bồi thường với bên thứ Ba, Bảo hiểm PVI sẽ chế tài một phần
+          số tiền bồi thường tương ứng với phần trách nhiệm của bên thứ ba.
+        </p>
+
+        <h3>BỒI THƯỜNG BẢO HIỂM TRÁCH NHIỆM DÂN SỰ CHỦ XE CƠ GIỚI</h3>
+
+        <p>
+          Khi không may xảy ra tai nạn gây thiệt hại cho người thứ ba, quý khách
+          thực hiện các bước sau:
+        </p>
+
+        <h5>Bước 1:</h5>
+
+        <p>
+          Thông báo ngay cho Bảo hiểm PVI qua đường dây nóng 1900545458 và thực
+          hiện theo các chỉ dẫn của Bảo hiểm PVI.
+        </p>
+
+        <p>
+          Đồng thời, quý khách thực hiện ngay việc làm cần thiết để ngăn ngừa
+          tổn thất xảy ra thêm như cứu chữa, hạn chế thiệt hại về người và tài
+          sản, bảo vệ hiện trường; đồng thời thông báo cho cơ quan Công an hoặc
+          chính quyền địa phương nơi gần nhất.
+        </p>
+
+        <h5>Bước 2:</h5>
+
+        <h6>
+          1. Cung cấp các tài liệu, giấy tờ sau theo hướng dẫn của Bảo hiểm PVI
+        </h6>
+
+        <ul>
+          <li>Giấy đăng ký xe;</li>
+          <li>Giấy phép lái xe;</li>
+          <li>
+            Giấy chứng minh nhân dân hoặc Hộ chiếu hoặc các giấy tờ tùy thân
+            khác của lái xe;
+          </li>
+          <li>Giấy chứng nhận bảo hiểm;</li>
+        </ul>
+
+        <h6>
+          2. Phối hợp với Bảo hiểm PVI và nạn nhân thu thập các giấy tờ liên
+          quan cần thiết, bao gồm hoặc một số các tài liệu:
+        </h6>
+
+        <p>Tài liệu chứng minh thiệt hại về người</p>
+
+        <ul>
+          <li>Giấy chứng thương;</li>
+          <li>Giấy ra viện;</li>
+          <li>Giấy chứng nhận phẫu thuật;</li>
+          <li>Hồ sơ bệnh án;</li>
+          <li>Giấy chứng tử (trong trường hợp nạn nhân tử vong);</li>
+        </ul>
+
+        <p>Tài liệu chứng minh thiệt hại về tài sản:</p>
+
+        <ul>
+          <li>
+            Hóa đơn, chứng từ hợp lệ về việc sửa chữa, thay mới tài sản bị thiệt
+            hại do tai nạn giao thông gây ra do chủ xe thực hiện tại các cơ sở
+            do Bảo hiểm PVI chỉ định hoặc được sự đồng ý của Bảo hiểm PVI.
+          </li>
+          <li>
+            Các giấy tờ chứng minh chi phí cần thiết và hợp lý mà chủ xe đã chi
+            ra để giảm thiểu tổn thất hay để thực hiện theo chỉ dẫn của Bảo hiểm
+            PVI
+          </li>
+        </ul>
+
+        <p>Các tài liệu có liên quan đến vụ tai nạn (nếu có).</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -15,3 +151,9 @@ export default {
   layout: "calculator"
 };
 </script>
+
+<style lang="scss" scoped>
+:is(h1, h2, h3, h4, h5, h6, p) {
+  line-height: inherit !important;
+}
+</style>
