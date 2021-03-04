233df3a77321cd2f612ded2bb4b4667a52430141
Tran Duy An Khuong <dbcgdhh@gmail.com>
2021-03-04 21:30:40 +0700

    Update about us content (#43)

--- pages/gioi-thieu.vue
+++ pages/gioi-thieu.vue
@@ -2,13 +2,51 @@
   <section class="section">
     <div class="container">
       <div class="content">
-        <h3>ABOUT US</h3>
-        <p v-for="i in 4" :key="i">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-          temporibus suscipit corporis repudiandae molestiae consectetur
-          molestias eius, facilis minus perspiciatis accusantium blanditiis vel
-          culpa provident, enim recusandae nemo nostrum autem.
+        <h3>GIỚI THIỆU</h3>
+
+        <p>
+          Theo thông kê tại Việt Nam tính tới thời điểm hiện tại trên thị trường
+          có hơn 30 công ty bảo hiểm với rất nhiều sản phẩm bảo hiểm phi nhân
+          thọ rất đa dạng. Nếu tính luôn cả các chi nhánh của các công ty này có
+          lẽ số lượng đơn vị cung cấp bảo hiểm phải đến hàng trăm... Chính vì có
+          quá nhiều sự lựa chọn từ quá nhiều công ty, vô hình chung làm cho
+          người sử dụng dịch vụ, người có nhu cầu mua bảo hiểm cũng không biết
+          chọn công ty nào để đúng mục đích cuối cùng của người tiêu dùng tựu
+          trung cũng là công ty uy tín, sản phẩm đúng nhu cầu và giá cả phù hợp.
         </p>
+
+        <p>
+          Hiểu được khó khăn đó của người tiêu dùng
+          <a href="https://cuahangbaohiem.com">cuahangbaohiem.com</a> (CHBH) ra
+          đời như là 1 nơi tổng hợp và cô đọng các sản phẩm từ các công ty theo
+          cách tối ưu nhất, dễ hiểu nhất để khách hàng có thể chắc lọc và lựa
+          chọn sản phẩm phù hợp với nhu cầu của mình nhất. Ngay khi nghĩ đến nhu
+          cầu tham gia bất kỳ sản phẩm bảo hiểm nào khách hàng chỉ cần đến
+          website
+          <a href="https://cuahangbaohiem.com">cuahangbaohiem.com</a> với vài cú
+          click chuột sản phẩm sẽ được trao đến tận tay của khách hàng.
+        </p>
+
+        <p>
+          <a href="https://cuahangbaohiem.com">cuahangbaohiem.com</a> xây dưng
+          trên nền tảng công nghệ mới với cam kết:
+        </p>
+
+        <ul>
+          <li>Phân phối các sản phẩm từ các nhà bảo hiểm uy tín.</li>
+          <li>
+            Báo phí bảo hiểm nhanh nhất của công ty bảo hiểm hàng đầu để khách
+            hàng dễ so sánh.
+          </li>
+          <li>Sản phẩm phong phú đa dạng, thao tác đơn giản.</li>
+          <li>Cung cấp sản phẩm tức thời với công nghệ cấp hợp đồng online.</li>
+          <li>
+            Chế độ hậu mãi và chăm sóc khách hàng tận tình. Hỗ trợ sự cố 24/7
+            Với những sự tiện lợi của CHBH giúp các bạn chỉ cần ngồi ở nhà chọn
+            lựa sản phẩm bảo hiểm, xây dựng vòng tròn tài chính an toàn cho bản
+            thân và gia đình.
+          </li>
+        </ul>
       </div>
     </div>
   </section>
