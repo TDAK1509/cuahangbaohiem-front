bca8b238a7f9abbfb580cd209676807f9079112a
Khuong Tran <dbcgdhh@gmail.com>
2021-03-05 19:33:34 +0700

    Use vue-carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home container">
-    <agile :nav-buttons="false" autoplay>
-      <figure class="home__slide">
-        <img class="home__img" src="~/assets/images/slide-1.png" />
-      </figure>
-
-      <figure class="home__slide">
-        <img class="home__img" src="~/assets/images/slide-2.png" />
-      </figure>
-    </agile>
+    <client-only>
+      <vue-carousel v-bind="carouselOptions">
+        <vue-carousel-slide>
+          <img class="home__img" src="~/assets/images/slide-1.png" />
+        </vue-carousel-slide>
+        <vue-carousel-slide>
+          <img class="home__img" src="~/assets/images/slide-2.png" />
+        </vue-carousel-slide>
+      </vue-carousel>
+    </client-only>
   </div>
 </template>
 
@@ -20,14 +21,14 @@ export default Vue.extend({
 
   data() {
     return {
-      sliderHeight: 0
+      carouselOptions: {
+        perPage: 1,
+        autoplay: true,
+        centerMode: true,
+        loop: true,
+        paginationEnabled: false
+      }
     };
-  },
-
-  mounted() {
-    const headerHeight = 75;
-    const footerHeight = 138;
-    this.sliderHeight = window.innerHeight - headerHeight - footerHeight;
   },
 
   head() {
@@ -45,36 +46,15 @@ export default Vue.extend({
   z-index: 0;
 }
 
-.home__slide {
-  width: 500px;
-  height: 666px;
-  display: flex;
-  justify-content: center;
-}
+// .home__slide {
+//   width: 500px;
+//   height: 666px;
+//   display: flex;
+//   justify-content: center;
+// }
 
-.home__img {
-  height: 100%;
-  object-fit: cover;
-}
-
-@media only screen and (max-width: 1200px) {
-  .home__slide {
-    width: 580px;
-    height: 773px;
-  }
-}
-
-@media only screen and (max-width: $mobile) {
-  .home__slide {
-    width: 300px;
-    height: 400px;
-  }
-}
-
-@media only screen and (max-width: 374px) {
-  .home__slide {
-    width: 250px;
-    height: 333px;
-  }
-}
+// .home__img {
+//   height: 100%;
+//   object-fit: cover;
+// }
 </style>
