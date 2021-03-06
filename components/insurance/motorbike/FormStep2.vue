6681104d2823263210c204dc99635bea5786e544
Khuong Tran <dbcgdhh@gmail.com>
2021-03-06 09:41:23 +0700

    Make test pass

--- components/insurance/motorbike/FormStep2.vue
+++ components/insurance/motorbike/FormStep2.vue
@@ -8,7 +8,12 @@
         pattern="[a-zA-Z\s]+"
         title="Tên của bạn có số hoặc kí tự lạ."
       />
-      <TextField data-cy="license-plate-input" required />
+      <TextField
+        data-cy="license-plate-input"
+        required
+        pattern="[0-9]{2}[A-Z][0-9]-[0-9]{4,5}"
+        title="Điền biển số xe theo mẫu: 55Z5-1234 hoặc 66Z6-123456."
+      />
 
       <button data-cy="next-button">NEXT</button>
     </form>
