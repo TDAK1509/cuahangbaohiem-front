75d3ba971f34fe2cd8ab3df335b88a3b60a3e716
Khuong Tran <dbcgdhh@gmail.com>
2021-03-13 10:29:37 +0700

    Add thanh toan page

---
+++ pages/san-pham/xe-may/thanh-toan.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <div class="container">
+      Vui lòng chuyển khoản vào số tài khoản: 12341451234.
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ThanhToanXeMay"
+};
+</script>
+
+<style>
+</style>
