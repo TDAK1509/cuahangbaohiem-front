71dbe3b93da06c33d29d8f33627c0817d7899d49
Khuong Tran <dbcgdhh@gmail.com>
2021-03-22 13:58:57 +0700

    Add change year test

--- test/e2e/integration/tinh-phi-xe-may.spec.js
+++ test/e2e/integration/tinh-phi-xe-may.spec.js
@@ -11,14 +11,24 @@ describe("Tinh phi xe may", () => {
     checkInsuranceFeeValue(toVnd(66000));
   });
 
-  function checkInsuranceFeeValue(value) {
-    cy.get("[data-cy=insurance-fee]").find("input").should("have.value", value);
-  }
+  it("Change year updates insurance value", () => {
+    checkInsuranceFeeValue(toVnd(66000));
+    cy.get("select").eq(1).select("2 năm");
+    checkInsuranceFeeValue(toVnd(66000 * 2));
 
-  function toVnd(money) {
-    return money.toLocaleString("it-IT", {
-      style: "currency",
-      currency: "VND"
-    });
-  }
+    cy.get("select").first().select("Dưới 50cc");
+    cy.get("select").eq(1).select("3 năm");
+    checkInsuranceFeeValue(toVnd(60500 * 3));
+  });
 });
+
+function checkInsuranceFeeValue(value) {
+  cy.get("[data-cy=insurance-fee]").find("input").should("have.value", value);
+}
+
+function toVnd(money) {
+  return money.toLocaleString("it-IT", {
+    style: "currency",
+    currency: "VND"
+  });
+}
